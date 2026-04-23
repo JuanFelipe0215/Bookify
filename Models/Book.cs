6eb5d7279c6fd144b3f0cf1b362b605652a49421
Juan Felipe Cardona Rios <juanfe13q@gmail.com>
2026-04-22 19:20:09 -0500

    feat: Properties of the Book Entity

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,6 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Bookify.Models;
 
 public class Book
 {
+    public int Id { get; set; }
     
+    [Required (ErrorMessage = "Nombre del libro requerido")]
+    public string Title { get; set; }
+    
+    [Required (ErrorMessage = "Autor requerido")]
+    public string Author { get; set; }
+    
+    [Required(ErrorMessage = "Codigo unico identificador de libro incorrecto")]
+    public string Isbn { get; set; }
+    
+    [Required(ErrorMessage = "Ingrese un tipo de Género")]
+    public BookGenre BookGenre { get; set; }
+    
+    public int? PublicationYear { get; set; }
+    
+    [Range(1, int.MaxValue, ErrorMessage = "Debe ser mayor a 0")]
+    public int QuantityAvailable { get; set; }
+
+    public ICollection<Loan> Loans { get; set; } = new List<Loan>();
 }
