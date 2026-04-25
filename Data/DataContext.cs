5c8cbfb979bd36ab14354b5b195b8c70acdec25c
Juan Felipe Cardona Rios <juanfe13q@gmail.com>
2026-04-25 09:47:12 -0500

    feat: Add dbset

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,6 +1,36 @@
+using Bookify.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace Bookify.Data;
 
-public class DataContext
+public class DataContext:DbContext
 {
+
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+        
+    }
     
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Loan> Loans { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
+        
+        modelBuilder.Entity<Loan>()
+            .HasOne(l => l.User)
+            .WithMany(u => u.Loans)
+            .HasForeignKey(l => l.IdUser)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Loan>()
+            .HasOne(l => l.Book)
+            .WithMany(b => b.Loans)
+            .HasForeignKey(l => l.IdBook)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
 }
