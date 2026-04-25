0fffa8baec6d5426b1ee7fa9bd85a93355790c90
Juan Felipe Cardona Rios <juanfe13q@gmail.com>
2026-04-25 09:44:16 -0500

    feat: create ServicesResponse

--- Response/ServiceResponse.cs
+++ Response/ServiceResponse.cs
@@ -1,6 +1,11 @@
 namespace Bookify.Response;
 
-public class ServiceResponse
+public class ServiceResponse<T>
 {
+    public T Data { get; set; }
+    
+    public bool Success { get; set; }
+    
+    public string? Message { get; set; }
     
 }
